b89122ee27cc24ced391738e1c19491aa7f61739
tommyjanna <25873341+tommyjanna@users.noreply.github.com>
2020-11-01 16:39:16 -0500

    Greatest commit to ever bless your eyes.

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from nnf import true
 
 
 N_AIRPORTS = 3
-N_TIMESTEPS = 2
+N_TIMESTEPS = 2 
 
 pilot_a = []
 pilot_b = []
@@ -60,15 +60,6 @@ def display_solution(solution):
             print("Airport " + str(flight) + ": " + str(solution[keys[flight + (N_AIRPORTS * timestep)]]) + str(pilot_a[timestep][flight]))
     """
 
-
-def recursionAirports(airports):
-    if(airports > 0): 
-        return (pilot_a[N_TIMESTEPS][airports] & ~recursionAirports(airports - 1))
-
-def recursionTimesteps(timesteps):
-    if(0 > timesteps): 
-        return (pilot_a[timesteps][N_AIRPORTS] & ~recursionTimesteps(timesteps - 1))
-
 #
 # Build an example full theory for your setting and return it.
 #
@@ -78,46 +69,49 @@ def recursionTimesteps(timesteps):
 def example_theory():
     E = Encoding()
     
-    # Pilot A must make exactly 2 flights.
-    """for flight in range(N_AIRPORTS):
-        
-        if pilot_a[i] == true:
-            flight_sum += 1
-    """
+    # Use a string to build the logical expression dynamically.
+    only_one_airport = ""
 
     for timestep in range(N_TIMESTEPS):
-        E.add_constraint((pilot_a[timestep][0] & ~pilot_a[timestep][1] & ~pilot_a[timestep][2]) | (~pilot_a[timestep][0] & pilot_a[timestep][1] & ~pilot_a[timestep][2]) | (~pilot_a[timestep][0] & ~pilot_a[timestep][1] & pilot_a[timestep][2]))
-        """ CNF
-        E.add_constraint(~pilot_a[timestep][0] | ~pilot_a[timestep][1])
-        E.add_constraint(~pilot_a[timestep][0] | ~pilot_a[timestep][2])
-        E.add_constraint(~pilot_a[timestep][0] | pilot_a[timestep][1] | ~pilot_a[timestep][2])
-        E.add_constraint(pilot_a[timestep][0] | ~pilot_a[timestep][1] | ~pilot_a[timestep][2])
-        E.add_constraint(~pilot_a[timestep][1] | ~pilot_a[timestep][2])
-        E.add_constraint(pilot_a[timestep][0] | pilot_a[timestep][1] | pilot_a[timestep][2])
-        """
+        only_one_airport += "("
+        for airport in range(N_AIRPORTS):
+            # Current airport in loop is true.
+            only_one_airport += "(pilot_a[%d][%d] &" % (timestep, airport)
 
-    #E.add_constraint((pilot_a[0][0] & ~pilot_a[0][1] & ~pilot_a[0][2]) | (~pilot_a[0][0] & pilot_a[0][1] & ~pilot_a[0][2]) | (~pilot_a[0][0] & ~pilot_a[0][1] & pilot_a[0][2]))
+            # All airports below airport are false, count up
+            for count_up in range(0, airport):
+                only_one_airport += " ~pilot_a[%d][%d] &" % (timestep, count_up)
+
+            # All airports above airport are false, count up
+            for count_up in range(airport + 1, N_AIRPORTS):
+                only_one_airport += " ~pilot_a[%d][%d] &" % (timestep, count_up)
+
+            # Remove the last & from the string
+            only_one_airport = only_one_airport[:-2]
+            only_one_airport += ") | "
+        
+        # Remove the last | from the string, use &'s to seperate timesteps
+        only_one_airport = only_one_airport[:-3]
+        only_one_airport += ") & "
+
+    # Remove the last & from the string.
+    only_one_airport = only_one_airport[:-3] 
+    
+
+    # Some sexy stuff 
+    print(only_one_airport)
+    print(eval(only_one_airport))
+
+    # Evaluate the string to turn it into NNF type
+    E.add_constraint(eval(only_one_airport))
 
     # Pilot A always starts on airport 1!
     # E.add_constraint(pilot_a[0][0])
 
-    # Pilot A cannot travel to the same airport in its 2 timesteps.
-    E.add_constraint((~pilot_a[0][0] | ~pilot_a[1][0]) & (~pilot_a[0][1] | ~pilot_a[1][1]) & (~pilot_a[0][2] | ~pilot_a[1][2]))
-
-    #T T F
-    #F F T
-
-    #~(A & B)
-    #~A | ~B
-
-    #for i in range(N_AIRPORTS):
-    #    E.add_constraint((~pilot_a[0][i] & pilot_a[1][i]) | (pilot_a[0][i] & ~pilot_a[1][i]) | (~pilot_a[0][i] & ~pilot_a[1][i]))
-
-        #E.add_constraint((pilot_b[timestep][0] & ~pilot_b[timestep][1] & ~pilot_b[timestep][2]) | (~pilot_b[timestep][0] & pilot_b[timestep][1] & ~pilot_b[timestep][2]) | (~pilot_b[timestep][0] & ~pilot_b[timestep][1] & pilot_b[timestep][2]))
-
-    # E.add_constraint((pilot_a[0] & pilot_a[1] & ~pilot_a[2]) | (pilot_a[0] & pilot_a[2] & ~pilot_a[1]) | (pilot_a[1] & pilot_a[2] & ~pilot_a[0]))
-    
-    # Pilot A and Pilot B must end at different airports.
+    # The pilot cannot be at the same airport in two adjacent timesteps.
+    for timestep in range(N_TIMESTEPS - 1):
+        for airport in range(N_AIRPORTS):
+            E.add_constraint(~pilot_a[timestep][airport] | ~pilot_a[timestep + 1][airport])
 
     return E
 
